a92ceccb4f5a4d453c6f7d406b2ea3b2b3429abc
Ubuntu <ubuntu@ip-172-31-27-190.us-east-2.compute.internal>
2021-11-19 07:02:32 +0000

    i

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :ruby_employee_apps
+  root 'ruby_employee_apps#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
